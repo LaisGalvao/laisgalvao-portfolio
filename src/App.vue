2f58668244d6ed47915a9681ffdbd1b6083adb93
Laís Galvão <laisgbueno62@gmail.com>
2024-11-27 21:48:29 +0000

    componentes

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <NavbarComp />
+    <div class="container mt-5">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import NavbarComp from './components/NavbarComp.vue';
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  name: 'App',
+  components: {
+    NavbarComp
   }
-}
+};
+</script>
+
+<style>
+/* Estilos globais */
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ContentComp 
+      header="Página Inicial" 
+      bodyText="Aqui você pode ver uma introdução ao portfólio e projetos."
+    />
+    <CardComp 
+      title="Projeto 1"
+      content="Descrição do projeto 1"
+    />
+    <CardComp 
+      title="Projeto 2"
+      content="Descrição do projeto 2"
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ContentComp from '../components/ContentComp.vue';
+import CardComp from '../components/CardComp.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    ContentComp,
+    CardComp
   }
-}
+};
 </script>
+
+<style scoped>
+.home {
+  padding: 20px;
+}
+</style>
