67bee745c0bdf97ab53854bac8b312855c494fe6
Laís Galvão <laisgbueno62@gmail.com>
2025-01-06 17:35:48 +0000

    tá usando esse caralho agora

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,6 @@
 <template>
   <div class="home">
-    <ContentComp 
-      header="Página Inicial" 
-      bodyText="Aqui você pode ver uma introdução ao portfólio e projetos."
-    />
-    <CardComp 
-      title="Projeto 1"
-      content="Descrição do projeto 1"
-    />
-    <CardComp 
-      title="Projeto 2"
-      content="Descrição do projeto 2"
-    />
+    <HelloWorld />
   </div>
 </template>
 
